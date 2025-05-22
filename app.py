bbc163ca73e7903a80bb9b9e89dc3394969a16d0
Sainath Reddy <sainathreddy@Anuroops-MacBook-Air.local>
2025-05-22 10:17:39 -0400

    loginroute added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,request,jsonify
-
+import requests
 
 app = Flask(__name__)
 app.secret_key = 'sainath123567'
@@ -7,6 +7,12 @@ app.secret_key = 'sainath123567'
 @app.route('/')
 def index():
     return {"response":"Hello User"}
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    print("Hey user")
+    
+    return {"response":"Successfully loggedin"}
 
 @app.route("/dashboard",methods=['GET','POST'])
 def dashboard():
