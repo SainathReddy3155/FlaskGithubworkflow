ae9a409008585f994a6accef7116638505fc5770
Sainath Reddy <sainathreddy@Anuroops-MacBook-Air.local>
2025-10-20 11:31:19 -0400

    added testcases

--- testapp.py
+++ testapp.py
@@ -1,6 +1,38 @@
 import unittest
 from app import app
+import pytest
 
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_profile_sucess(client):
+    response=client.get("/profile?username=sainath&login_status=true")
+    data=response.get_json()
+    assert response.status_code=200
+    assert data["response"]=="This is your profile page"
+
+def test_profile_failure(client):
+    response=client.get("/profile?username=sainath&login_status=false")
+    data=response.get_json()
+    assert response.status_code==200
+    assert response['response'] == "Please login to access your profile"
+
+def test_username_notfound(client):
+    response=client.get("/profile?login_status=true")
+    data=response.get_json()
+    assert response.status_code==200
+    assert data["response"]=="Please login to access your profile"
+
+def test_login_status_notfound(client):
+    response=client.get("/profile?username=sainath")
+    data=response.get_json()
+    assert response.status_code==200
+    assert data["response"]=="Please login to access your profile"
 
 def test_home():
     print("In Test")
@@ -11,3 +43,4 @@ def test_dashboard_route():
     client=app.test_client()
     response=client.get("/dashboard")
     assert response.status_code==200
+
